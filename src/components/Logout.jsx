1abe7f0e370bddd54307dbb127a47e3242bce8be
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-11-15 23:05:00 +0100

    added fontawesome - not working ..

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth } from "./../context/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Logout() {
   const { authenticateUser } = useAuth();
@@ -10,7 +12,15 @@ function Logout() {
   };
   return (
     <>
-      <div onClick={handleLogout}>Logout</div>
+      <div onClick={handleLogout}>
+        <FontAwesomeIcon
+          icon={faArrowRightFromBracket}
+          style={{
+            "--fa-primary-color": "#bbee11",
+            "--fa-secondary-color": "#000001",
+          }}
+        />
+      </div>
     </>
   );
 }
